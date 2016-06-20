a1a73452c538695acc83ecf9aca76d53e82f9d98
marcogil93 <marcogil93@gmail.com>
2016-06-20 15:06:50 -0500

    add-script login face

---
+++ Scripts/login.cs
@@ -0,0 +1,108 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using Facebook.Unity;
+
+public class login : MonoBehaviour
+{
+	public GameObject DialogLoggedIn;
+	public GameObject DialogLoggedOut;
+	public GameObject DialogUsername;
+	public GameObject DialogProfilePic;
+
+    // Use this for initialization
+    void Awake()
+    {
+        FB.Init(setInit, OnHideUnity);
+
+		Debug.Log(FbManager.Instance.isLoggedIn);
+    }
+
+    void setInit()
+    {
+        if (FB.IsLoggedIn)
+        {
+            Debug.Log("FB is logged in");
+        }
+        else
+        {
+            Debug.Log("FB is not logged in");
+        }
+
+		DealWithFBMenus (FB.IsLoggedIn);
+    }
+
+    void OnHideUnity(bool isGameShown)
+    {
+        if (isGameShown)
+        {
+            Time.timeScale = 0;
+        }
+        else
+        {
+            Time.timeScale = 1;
+        }
+    }
+
+    public void FBlogin()
+    {
+        List<string> permissions = new List<string>();
+        permissions.Add("public_profile");
+
+        FB.LogInWithReadPermissions(permissions, AuthCallback);
+    }
+
+    void AuthCallback(IResult result)
+    {
+        if (result.Error != null)
+        {
+            Debug.Log(result.Error);
+        }
+        else
+        {
+            if (FB.IsLoggedIn)
+            {
+                Debug.Log("FB is logged in");
+            }
+            else
+            {
+                Debug.Log("FB is not logged in");
+            }
+
+			DealWithFBMenus (FB.IsLoggedIn);
+        }
+    }
+
+	void DealWithFBMenus (bool isLoggedIn) {
+		if (isLoggedIn) {
+			DialogLoggedIn.SetActive (true);
+			DialogLoggedOut.SetActive (false);
+
+			FB.API ("/me?fields=first_name", HttpMethod.GET, DisplayUsername);
+			FB.API ("/me/picture?type=square&height=128&Width=128", HttpMethod.GET, DisplayProfilePic);
+		} else {
+			DialogLoggedIn.SetActive (false);
+			DialogLoggedOut.SetActive (true);
+		}
+	}
+
+	void DisplayUsername(IResult result) {
+		Text UserName = DialogUsername.GetComponent<Text> ();
+
+		if (result.Error == null) {
+			UserName.text = "Hola " + result.ResultDictionary ["first_name"]; 
+		} else {
+			Debug.Log (result.Error);
+		}
+	}
+
+	void DisplayProfilePic(IGraphResult result) {
+		if (result.Texture != null) {
+			Image ProfilePic = DialogProfilePic.GetComponent<Image> ();
+
+			ProfilePic.sprite = Sprite.Create (result.Texture, new Rect (0, 0, 128, 128), new Vector2 ());
+		}
+	}
+		
+}

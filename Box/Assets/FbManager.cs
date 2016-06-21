2c8ddfafeb9e5baa4d6509d5d7f8297cef2437b6
marcogil93 <marcogil93@gmail.com>
2016-06-21 06:19:12 -0500

    facebook

--- Box/Assets/FbManager.cs
+++ Box/Assets/FbManager.cs
@@ -1,12 +1,154 @@
-﻿using System;
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System;
+using System.Collections.Generic;
+using Facebook.Unity;
 
-namespace AssemblyCSharp
-{
-	public class FbManager
-	{
-		public FbManager ()
+
+public class FbManager : MonoBehaviour {
+	public static FbManager _instance;
+
+	public static FbManager Instance {
+		get { 
+			if (_instance == null) {
+				GameObject fbm = new GameObject ("FBManager");
+				fbm.AddComponent<FbManager> ();
+			}
+
+			return _instance;
+		}
+	}
+
+	public bool IsLoggedIn    { get; set;}
+	public string ProfileName { get; set;}
+	public Sprite ProfilePic  { get; set;}
+	public string AppLinkUrl  { get; set;}
+
+
+	void Awake() {
+		DontDestroyOnLoad (this.gameObject);
+		_instance = this;
+
+		IsLoggedIn = true;
+	}
+
+	public void InitFB() {
+		if (!FB.IsInitialized) {
+			FB.Init (setInit, OnHideUnity);	
+		} else {
+			IsLoggedIn = FB.IsLoggedIn;
+		}
+	}
+
+	void setInit() {
+		if (FB.IsLoggedIn)
 		{
+			Debug.Log("FB is logged in");
+			GetProfile ();
+		}
+		else
+		{
+			Debug.Log("FB is not logged in");
+		}
+
+		IsLoggedIn = FB.IsLoggedIn;
+	}
+
+	void OnHideUnity(bool isGameShown) {
+		if (isGameShown)
+		{
+			Time.timeScale = 0;
+		}
+		else
+		{
+			Time.timeScale = 1;
+		}
+	}
+
+	public void GetProfile() {
+		FB.API ("/me?fields=first_name", HttpMethod.GET, DisplayUsername);
+		FB.API ("/me/picture?type=square&height=128&Width=128", HttpMethod.GET, DisplayProfilePic);
+		FB.GetAppLink (DealWithAppLink);
+	}
+
+	void DisplayUsername(IResult result) {
+
+		if (result.Error == null) {
+			ProfileName = "" + result.ResultDictionary ["first_name"]; 
+		} else {
+			Debug.Log (result.Error);
+		}
+	}
+
+	void DisplayProfilePic(IGraphResult result) {
+		if (result.Texture != null) {
+			ProfilePic = Sprite.Create (result.Texture, new Rect (0, 0, 128, 128), new Vector2 ());
+		}
+	}
+
+	void DealWithAppLink(IAppLinkResult result) {
+		if (!string.IsNullOrEmpty (result.Url)) {
+			AppLinkUrl = result.Url;
+			Debug.Log (AppLinkUrl);
+		}
+	}
+
+	public void Share() {
+		FB.ShareLink(
+			new Uri("https://developers.facebook.com/"),
+			callback: ShareCallback
+		);
+	}
+
+	void ShareCallback (IResult result) {
+		if (result.Cancelled) {
+			Debug.Log ("Share Cancelled");
+		} else if (!string.IsNullOrEmpty (result.Error)) {
+			Debug.Log ("Error on Share");
+		} else if(!string.IsNullOrEmpty (result.RawResult)){
+			Debug.Log ("Success on Share");
+		}
+	}
+
+	public void Invite() {
+		FB.Mobile.AppInvite (
+			new Uri(AppLinkUrl),
+			null,
+			InviteCallback
+		);
+	}
+
+	void InviteCallback(IResult result) {
+		if (result.Cancelled) {
+			Debug.Log ("Invite Cancelled");
+		} else if (!string.IsNullOrEmpty (result.Error)) {
+			Debug.Log ("Error on Invite");
+		} else if(!string.IsNullOrEmpty (result.RawResult)){
+			Debug.Log ("Success on Invite");
+		}
+	}
+
+	public void ShareWithUsers() {
+		FB.AppRequest (
+			"Unete",
+			new List<string>() { "1174721656"},
+			null,
+			null,
+			10,
+			"data",
+			"title",
+			ShareWithUsersCallback
+		);
+	}
+
+	void ShareWithUsersCallback(IAppRequestResult result) {
+		if (result.Cancelled) {
+			Debug.Log ("Challenge Cancelled");
+		} else if (!string.IsNullOrEmpty (result.Error)) {
+			Debug.Log ("Error on Challenge");
+		} else if(!string.IsNullOrEmpty (result.RawResult)){
+			Debug.Log (result.RawResult);
 		}
 	}
 }
-
---
+++ Box/Assets/login.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using Facebook.Unity;
+
+public class login : MonoBehaviour
+{
+	public GameObject DialogLoggedIn;
+	public GameObject DialogLoggedOut;
+	public GameObject DialogUsername;
+	public GameObject DialogProfilePic;
+
+	// Use this for initialization
+	void Awake() {
+		FbManager.Instance.InitFB ();
+		DealWithFBMenus (FB.IsLoggedIn);
+	}
+
+	public void FBlogin() {
+		List<string> permissions = new List<string>();
+		//permissions.Add("public_profile");
+		permissions.Add("publish_actions");
+
+		FB.LogInWithPublishPermissions(permissions, AuthCallback);
+	}
+
+	void AuthCallback(IResult result) {
+		if (result.Error != null) {
+			Debug.Log(result.Error);
+		} else {
+			if (FB.IsLoggedIn) {
+				FbManager.Instance.IsLoggedIn = true;
+				FbManager.Instance.GetProfile ();
+				Debug.Log("FB is logged in");
+			} else {
+				Debug.Log("FB is not logged in");
+			}
+
+			DealWithFBMenus (FB.IsLoggedIn);
+		}
+	}
+
+	void DealWithFBMenus (bool isLoggedIn) {
+		if (isLoggedIn) {
+			DialogLoggedIn.SetActive (true);
+			DialogLoggedOut.SetActive (false);
+
+			if (FbManager.Instance.ProfileName != null) {
+				Text UserName = DialogUsername.GetComponent<Text> ();
+				UserName.text = "Hi, " + FbManager.Instance.ProfileName;
+			} else {
+				StartCoroutine ("WaitForProfileName");
+			}
+
+			if (FbManager.Instance.ProfilePic != null) {
+				Image ProfilePic = DialogProfilePic.GetComponent<Image> ();
+				ProfilePic.sprite = FbManager.Instance.ProfilePic;
+			} else {
+				StartCoroutine ("WaitForProfilePic");
+			}
+		} else {
+			DialogLoggedIn.SetActive (false);
+			DialogLoggedOut.SetActive (true);
+		}
+	}
+
+	IEnumerator WaitForProfileName() {
+		while(FbManager.Instance.ProfileName == null) {
+			yield return null;
+		}
+
+		DealWithFBMenus (FbManager.Instance.IsLoggedIn);
+	}
+
+	IEnumerator WaitForProfilePic() {
+		while(FbManager.Instance.ProfilePic == null) {
+			yield return null;
+		}
+
+		DealWithFBMenus (FbManager.Instance.IsLoggedIn);
+	}
+
+	public void Share() {
+		FbManager.Instance.Share ();
+	}
+
+	public void Invite() {
+		FbManager.Instance.Invite ();
+	}
+
+	public void ShareWithUsers() {
+		FbManager.Instance.ShareWithUsers ();
+	}
+
+}
